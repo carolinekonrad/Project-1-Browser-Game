19e45916f448f13695511577707602aa870937f7
Caroline Konrad <ckonrad@live.unc.edu>
2022-04-16 13:14:33 -0400

    Adding bet page

---
+++ public/app.js
@@ -0,0 +1,8 @@
+//selecting bet chips 
+var bet = 0
+var totalBet = document.getElementById('total-bet')
+
+let redButton = document.getElementById("red")
+redButton.addEventListener('click', function(){
+    console.log('hello, world')
+})
